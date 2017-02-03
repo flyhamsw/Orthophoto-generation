35a641bdf5de576cad23f7c81c0970cdd745f194
Sangwoo Ham <flyhamsw@gmail.com>
2017-02-03 14:25:43 +0900

    After the experiment at Hangang at Feb 1, some codes in ApxModifier were changed for input of abnormal APX file.

--- ApxModifier.cpp
+++ ApxModifier.cpp
@@ -6,8 +6,13 @@
 #include <sstream>
 #include <queue>
 #include <stdlib.h>
+#include <math.h>
+#include <string>
 
 using namespace std;
+
+//Constant value for pi
+const double pi = 3.141592653589793238463;
 
 ApxModifier::ApxModifier(char* txt_filename)
 {
@@ -22,18 +27,21 @@ void ApxModifier::loadData()
 	cout << "=========================================================" << endl << "Loading file (" << filename << ") ...";
 	if (f.is_open())
 	{
-		char line[100];
+		//Split data and store to RowGPGGA, RowPASHR, RowPTNL
+		char line[300];
+
 		while (!f.eof())
 		{
+			//Get a line and store to a stringstream
 			f.getline(line, sizeof(line));
 			stringstream line_stream(line);
 
 			while (!line_stream.eof())
 			{
+				//In a stringstream, get a comma separated word
+				//Determine whether it is GPS data or INS Data
 				char* lineHeader = new char[20];
 				line_stream.getline(lineHeader, 20, ',');
-
-				//cout << lineHeader << endl;
 
 				if (strcmp(lineHeader, "$GPGGA") == 0)
 				{
@@ -136,6 +144,7 @@ void ApxModifier::loadData()
 
 void ApxModifier::matchData()
 {
+	//Find GPS data and INS data with same time, and conjugate the GPS-INS data
 	cout << "Matching Data...";
 
 	while (!(qGPGGA.empty()) && !(qPASHR.empty()))
@@ -149,6 +158,7 @@ void ApxModifier::matchData()
 		time_GPGGA = atof(currRowGPGGA->time);
 		time_PASHR = atof(currRowPASHR->time);
 
+		//Pop queue with lower time value
 		if (time_GPGGA < time_PASHR)
 		{
 			qGPGGA.pop();
@@ -157,6 +167,7 @@ void ApxModifier::matchData()
 		{
 			qPASHR.pop();
 		}
+		//If time data of each queue are same, store them, and pop both queues
 		else if (time_GPGGA == time_PASHR)
 		{
 			Row* matchedRow = new Row(currRowGPGGA, currRowPASHR);
@@ -170,76 +181,174 @@ void ApxModifier::matchData()
 	cout << "# of Result Queue: " << qResult.size() << endl << endl;
 }
 
-void ApxModifier::findAdjacentData()
+//Copyright: http://m.blog.naver.com/tacma/20108668315
+void convertWGS84_to_TM(double *plon, double *plat)
+{
+	double lon, lat;
+	lon = *plon;
+	lat = *plat;
+	double m_arScaleFactor = 1;
+	double m_arLonCenter = 127 * pi / 180; //Projection center of Korean central belt 2010
+	double m_arLatCenter = 38 * pi / 180; //Projection center of Korean central belt 2010
+	double m_arFalseNorthing = 600000.0;
+	double m_arFalseEasting = 200000.0;
+	double x, y;
+	double m_dDstInd;
+	double m_arMajor = 6378137.0;
+	double m_arMinor = 6356752.3142;
+	double delta_lon; // Delta longitude (Given longitude - center longitude)
+	double sin_phi, cos_phi; // sin and cos value
+	double al, als; // temporary values
+	double b, c, t, tq; // temporary values
+	double con, n, ml; // cone constant, small m
+	double temp = m_arMinor / m_arMajor;
+	double m_ecc = 1.0 - temp * temp;
+	double m_eccp = (1 / temp)*(1 / temp) - 1;
+	double m_dDstE0 = 1.0 - 0.25 * m_ecc * (1.0 + m_ecc / 16.0 * (3.0 + 1.25 * m_ecc));
+	double m_dDstE1 = 0.375 * m_ecc * (1.0 + 0.25 * m_ecc * (1.0 + 0.46875 * m_ecc));
+	double m_dDstE2 = 0.05859375 * m_ecc * m_ecc * (1.0 + 0.75 * m_ecc);
+	double m_dDstE3 = m_ecc * m_ecc * m_ecc * (35.0 / 3072.0);
+	double m_dDstMl0 = m_arMajor * (m_dDstE0 * m_arLatCenter - m_dDstE1 * sin(2.0 * m_arLatCenter) + m_dDstE2 * sin(4.0 * m_arLatCenter) - m_dDstE3 * sin(6.0 * m_arLatCenter));
+	m_dDstInd = 0.0;
+	delta_lon = lon - m_arLonCenter;
+	sin_phi = sin(lat);
+	cos_phi = cos(lat);
+	b = 0;
+	x = 0.5 * m_arMajor * m_arScaleFactor * log((1.0 + b) / (1.0 - b));
+	con = acos(cos_phi * cos(delta_lon) / sqrt(1.0 - b * b));
+	al = cos_phi * delta_lon;
+	als = al * al;
+	c = m_eccp * cos_phi * cos_phi;
+	tq = tan(lat);
+	t = tq * tq;
+	con = 1.0 - m_ecc * sin_phi * sin_phi;
+	n = m_arMajor / sqrt(con);
+	ml = m_arMajor * (m_dDstE0 * lat - m_dDstE1 * sin(2.0 * lat) + m_dDstE2 * sin(4.0 * lat) - m_dDstE3 * sin(6.0 * lat));
+	x = m_arScaleFactor * n * al * (1.0 + als / 6.0 * (1.0 - t + c + als / 20.0 * (5.0 - 18.0 * t + t * t + 72.0 * c - 58.0 * m_eccp))) + m_arFalseEasting;
+	y = m_arScaleFactor * (ml - m_dDstMl0 + n * tq * (als * (0.5 + als / 24.0 * (5.0 - t + 9.0 * c + 4.0 * c * c + als / 30.0 * (61.0 - 58.0 * t + t * t + 600.0 * c - 330.0 * m_eccp))))) + m_arFalseNorthing;
+	*plon = x;
+	*plat = y;
+}
+
+void ApxModifier::interpolateData()
 {
 	cout << "Finding adjacent data...";
 
-	double takenTime = atof(qPTNL.front()->time);
-	double diffA;
-	double diffB;
-
-	int q_size = qResult.size();
-
-	//Find Location/Attitude data adjacent to the event
-	for (int i = 0; i < q_size - 1; i++)
+	if (qPTNL.size() == 0)
 	{
-		rowBefore = qResult.front();
-		qResult.pop();
-		rowAfter = qResult.front();
-
-		diffA = atof(rowBefore->rowGPGGA->time) - takenTime;
-		diffB = atof(rowAfter->rowGPGGA->time) - takenTime;
-
-		if (diffA * diffB < 0)
-		{
-			break;
-		}
+		cout << "No event data" << endl << endl;
 	}
-	cout << "OK" << endl;
-	cout << "The photo was taken between " << rowBefore->rowGPGGA->time << " and " << rowAfter->rowGPGGA->time << endl << endl;
-	cout << "Interpolation...";
+	else
+	{
+		double takenTime = atof(qPTNL.front()->time);
+		double diffA;
+		double diffB;
 
-	diffA = abs(diffA);
-	diffB = abs(diffB);
+		int q_size = qResult.size();
 
-	double weightA = 1 / diffA;
-	double weightB = 1 / diffB;
+		//Find Location/Attitude data adjacent to the event
+		for (int i = 0; i < q_size - 1; i++)
+		{
+			rowBefore = qResult.front();
+			qResult.pop();
+			rowAfter = qResult.front();
 
-	double weightedLat = (atof(rowBefore->rowGPGGA->lat)*weightA + atof(rowAfter->rowGPGGA->lat)*weightB) / (weightA + weightB);
-	double weightedLng = (atof(rowBefore->rowGPGGA->lng)*weightA + atof(rowAfter->rowGPGGA->lng)*weightB) / (weightA + weightB);
-	double weightedHeightWGS84 = (atof(rowBefore->rowGPGGA->heightWGS84)*weightA + atof(rowAfter->rowGPGGA->heightWGS84)*weightB) / (weightA + weightB);
-	double weightedHeading = (atof(rowBefore->rowPASHR->heading)*weightA + atof(rowAfter->rowPASHR->heading)*weightB) / (weightA + weightB);
-	double weightedRoll = (atof(rowBefore->rowPASHR->roll)*weightA + atof(rowAfter->rowPASHR->roll)*weightB) / (weightA + weightB);
-	double weightedPitch = (atof(rowBefore->rowPASHR->pitch)*weightA + atof(rowAfter->rowPASHR->pitch)*weightB) / (weightA + weightB);
+			diffA = atof(rowBefore->rowGPGGA->time) - takenTime;
+			diffB = atof(rowAfter->rowGPGGA->time) - takenTime;
 
-	rowInterpolated = new RowInterpolated(weightedLng, weightedLat, weightedHeightWGS84, weightedHeading, weightedRoll, weightedPitch);
+			//if diffA is negative and diffB is positive, the photo was taken between A and B
+			if (diffA * diffB < 0)
+			{
+				break;
+			}
+		}
+		cout << "OK" << endl;
+		cout << "The photo was taken between " << rowBefore->rowGPGGA->time << " and " << rowAfter->rowGPGGA->time << endl << endl;
+		cout << "Interpolation...";
 
-	cout << "OK" << endl << endl;
+		diffA = abs(diffA);
+		diffB = abs(diffB);
 
-	f.close();
+		//Interpolate by time difference
+		double weightA = 1 / diffA;
+		double weightB = 1 / diffB;
+
+		double weightedLat = (atof(rowBefore->rowGPGGA->lat)*weightA + atof(rowAfter->rowGPGGA->lat)*weightB) / (weightA + weightB);
+		double weightedLng = (atof(rowBefore->rowGPGGA->lng)*weightA + atof(rowAfter->rowGPGGA->lng)*weightB) / (weightA + weightB);
+		double weightedHeightWGS84 = (atof(rowBefore->rowGPGGA->heightWGS84)*weightA + atof(rowAfter->rowGPGGA->heightWGS84)*weightB) / (weightA + weightB);
+		double weightedHeading = (atof(rowBefore->rowPASHR->heading)*weightA + atof(rowAfter->rowPASHR->heading)*weightB) / (weightA + weightB);
+		double weightedRoll = (atof(rowBefore->rowPASHR->roll)*weightA + atof(rowAfter->rowPASHR->roll)*weightB) / (weightA + weightB);
+		double weightedPitch = (atof(rowBefore->rowPASHR->pitch)*weightA + atof(rowAfter->rowPASHR->pitch)*weightB) / (weightA + weightB);
+
+		//Split weighted values into Degree and Minute
+		double wLat_min = fmod(weightedLat, 100);
+		double wLat_deg = ((weightedLat - wLat_min) / 100);
+
+		double wLng_min = fmod(weightedLng, 100);
+		double wLng_deg = ((weightedLng - wLng_min) / 100);
+
+		//Convert Deg, Min values to Radian
+		weightedLat = (wLat_deg + wLat_min / 60) * pi / 180;
+		weightedLng = (wLng_deg + wLng_min / 60) * pi / 180;
+
+		convertWGS84_to_TM(&weightedLng, &weightedLat);
+
+		//Store interpolated data
+		rowInterpolated = new RowInterpolated(weightedLng, weightedLat, weightedHeightWGS84, weightedHeading, weightedRoll, weightedPitch);
+
+		cout << "OK" << endl << endl;
+
+		f.close();
+	}
+
 }
+
 
 void ApxModifier::writeNewFile(char* txt_filename)
 {
-	filename = txt_filename;
-
-	fnew.open(filename);
-	fnew.precision(8);
-	fnew.setf(ios::fixed);
-	fnew.setf(ios::showpoint);
-
-	cout << "Writing a new file (" << filename << ") ...";
-
-	if (fnew.is_open())
+	if (rowInterpolated == NULL)
 	{
-		fnew << rowInterpolated->X << '\t' << rowInterpolated->Y << '\t' << rowInterpolated->Z << '\t' << rowInterpolated->heading << '\t' << rowInterpolated->roll << '\t' << rowInterpolated->pitch;
+		cout << "Data was not interpolated" << endl;
+	}
+	else
+	{
+		filename = txt_filename;
+
+		int dotIdx;
+		int slIdx;
+
+		for (int i = strlen(filename); i >= 0; i--)
+		{
+			if (filename[i] == '.')
+			{
+				dotIdx = i;
+			}
+			else if (filename[i] == '/')
+			{
+				slIdx = i;
+				break;
+			}
+		}
+
+		string jpgName = filename;
+		jpgName = jpgName.substr(slIdx + 1, dotIdx - slIdx - 1);
+
+		fnew.open(filename);
+		fnew.precision(8);
+		fnew.setf(ios::fixed);
+		fnew.setf(ios::showpoint);
+
+		cout << "Writing a new file (" << filename << ") ...";
+
+		if (fnew.is_open())
+		{
+			fnew << jpgName << ".jpg" << '\t' << rowInterpolated->X << '\t' << rowInterpolated->Y << '\t' << rowInterpolated->Z << '\t' << rowInterpolated->heading << '\t' << rowInterpolated->roll << '\t' << rowInterpolated->pitch;
+		}
+
+		fnew.close();
+
+		cout << "OK" << endl;
+
 	}
 
-	fnew.close();
-
-	cout << "OK" << endl;
-}
-
-void ApxModifier::convertRawInterpolated2TM()
-{
 }
